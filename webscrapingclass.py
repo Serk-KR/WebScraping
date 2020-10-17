1d74f74ebf6a13d4799debdea913324d8e7199cb
Serk-KR <35376173+Serk-KR@users.noreply.github.com>
2020-10-17 21:14:24 +0200

    Resumen de las categorías y sus respectivas urls (hrefs) + Hecho que muestro objecto nav
    Conseguido hacer que se muestre todas las categorías padres (junto a las categorías hijas de cada uno) + sus respectivas hrefs (urls)

--- webscrapingclass.py
+++ webscrapingclass.py
@@ -10,6 +10,19 @@ from bs4 import BeautifulSoup as bs
 
 class WebScraping():
     
+    def __get_nav_names(self,nav):
+        print("PRINT NAV INICIO: -------")
+        print(nav)
+        print("PRINT NAV FIN: -------")
+        for idx, ultag in enumerate(nav.find_all('li')):
+            if len(ultag.find_all("li")) > 0:
+                #print("Inidice: " + str(idx))
+                print("")
+                categoriaPadre = ultag.find("a")
+                print(categoriaPadre.text + "-> href: " + str(categoriaPadre.get("href")))
+                for idx2, categoriaHijo in enumerate(ultag.find_all("li")):
+                    print(" -" +categoriaHijo.text + "-> href: " + str(categoriaHijo.find("a").get("href")))
+    
     def download_html(self, url):
         response = urlopen(url)
         html = response.read()
@@ -19,33 +32,13 @@ class WebScraping():
         content = bs(html, 'html.parser')
         nav_menu = content.find("nav", attrs={"class": "menu dark hero menuhead"})
         #menu_todo = nav_menu.find("div", attrs={"class": "middle"}).find("ul").find_all("li")[0]
-        menu_todo = nav_menu.find("div", attrs={"class": "middle"}).find_all("ul")[0]
-        menu_todo3 = nav_menu.find("div", attrs={"class": "middle"}).find_all("ul")
+        #menu_todo = nav_menu.find("div", attrs={"class": "middle"}).find_all("ul")[0]
+        menu_todo = nav_menu.find("div", attrs={"class": "middle"})
         
-        for idx, ultag in enumerate(menu_todo3):
-            print("Inidice: " + str(idx))
-            print("ultag-------------------")
-            print(ultag)
-            if idx == 0:
-                print(ultag.find_all("a", hrefs=True))
-            
-        
-        print("menu_todo")
-        print(menu_todo)
-        print(menu_todo.find_all('ul'))
-        for idx, ultag in enumerate(menu_todo.find_all('ul')):
-            print("Inidice: " + str(idx))
-            hrefs = ultag
-            print("hrefs")
-            print(hrefs)
-        #for idx, ultag in enumerate(menu.find_all('ul')):
-        #    print("Inidice: " + str(idx))
-        #
-        #    print("111111111111111111111111111111111111111111111111111111")
-        #    print(ultag)
-        #    for litag in ultag.find_all('li'):
-        #        print("2222222222222")
-        #        print("  - " + litag.text + ": " + litag.find("a").text)
+        # Mostramos las categorias del menú y su url
+        self.__get_nav_names(menu_todo)
+        return menu_todo
+
     
     def prueba(self):
         print("aaa")
