398c236ade9d669af293571e4cc02b1a18e5e901
luisleon1894 <luisleon1894@gmail.com>
2020-10-17 16:18:10 +0200

    se han creado funciones
    __download_html: descarga el html
    __get_nav_menu: ha de hacer return del nav_menu

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 """
 Created on Sat Oct 17 13:30:54 2020
 
-@author: Sergi
+@author: Sergi, Luis
 """
 
 # Hacemos los imports necesarios
 import requests
+from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 
 # Cargamos la información de la página web
@@ -19,19 +20,44 @@ if code_req != 200:
 else:
     print("Página web accesible")
 
-content = bs(pag_web.content)
+#content = bs(pag_web.content)
 #print(content.prettify)
 
-nav_menu = content.find("nav", attrs={"class": "menu dark hero menuhead"})
+#nav_menu = content.find("nav", attrs={"class": "menu dark hero menuhead"})
 #print(nav_menu.prettify)
 #menu = nav_menu.find("div", attrs={"class": "middle"}).find("ul")
-menu = nav_menu.find("div", attrs={"class": "middle"})
+#menu = nav_menu.find("div", attrs={"class": "middle"})
 #print(menu)
 
 
-for ultag in menu.find_all('ul'):
-    print("111111111111111111111111111111111111111111111111111111")
-    print(ultag)
-    for litag in ultag.find_all('li'):
-        print("2222222222222")
-        print("  - " + litag.text + ": " + litag.find("a").text)
+#for ultag in menu.find_all('ul'):
+ #   print("111111111111111111111111111111111111111111111111111111")
+  #  print(ultag)
+   # for litag in ultag.find_all('li'):
+    #    print("2222222222222")
+     #   print("  - " + litag.text + ": " + litag.find("a").text)
+     
+def __download_html(url):
+    response = urlopen(url)
+    html = response.read()
+    return html
+
+def __get_nav_menu(html):
+    content = bs(html, 'html.parser')
+    nav_menu = content.find("nav", attrs={"class": "menu dark hero menuhead"})
+    menu = nav_menu.find("div", attrs={"class": "middle"})
+    for ultag in menu.find_all('ul'):
+        print("111111111111111111111111111111111111111111111111111111")
+        print(ultag)
+        for litag in ultag.find_all('li'):
+            print("2222222222222")
+            print("  - " + litag.text + ": " + litag.find("a").text)
+
+
+# Download HTML
+html = __download_html("https://www.mustang.es/es/")
+beautsoup = bs(html, 'html.parser')
+
+
+# Get the names and links of navmenu
+navmenu_links = __get_nav_menu(html)
